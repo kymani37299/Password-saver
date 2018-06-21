11ae394253dc4edbbcb6c9e4425eefcec8f729d9
Marko Sreckovic <markosrecko97@hotmail.rs>
2018-06-21 16:42:37 +0200

    Build

--- build/build/src/app/AccountView.java
+++ build/build/src/app/AccountView.java
@@ -2,7 +2,6 @@ package app;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -18,24 +17,21 @@ public class AccountView extends GridPane{
 	private TextArea tfOther;
 	
 	private Button btnAction;
-	private Button actionButton;
 	
 	private AccountController accountController;
-	private Account account;
 	
-	public AccountView(AccountController accountController,Button actonButtton) {
+	public AccountView(AccountController accountController) {
 		this.setHgap(5);
 		this.setVgap(5);
 		
-		this.actionButton = actonButtton;
 		this.accountController = accountController;
 		
 		this.tfServer = new TextField();
 		this.tfUsername = new TextField();
-		this.tfPassword = new PasswordField();
+		this.tfPassword = new TextField(); //TODO: Implement show/hide button
 		this.tfEmail = new TextField();
 		this.tfOther = new TextArea();
-		this.btnAction = new Button("Edit");
+		this.btnAction = new Button("Add");
 		
 		this.tfServer.setMaxWidth(maxLength);
 		this.tfUsername.setMaxWidth(maxLength);
@@ -47,21 +43,22 @@ public class AccountView extends GridPane{
 		
 		this.add(new Label("Server: "), 0, 0);
 		this.add(new Label("Username: "), 0, 1);
-		this.add(new Label("Password: "), 0, 2);
-		this.add(new Label("Email: "), 0, 3);
+		this.add(new Label("Email: "), 0, 2);
+		this.add(new Label("Password: "), 0, 3);
 		this.add(new Label("Other: "), 0, 4);
 		
 		this.add(this.tfServer, 1, 0);
 		this.add(this.tfUsername, 1, 1);
-		this.add(this.tfPassword, 1, 2);
-		this.add(this.tfEmail, 1, 3);
+		this.add(this.tfEmail, 1, 2);
+		this.add(this.tfPassword, 1, 3);
 		this.add(this.tfOther, 1, 4);
 		
 		this.add(this.btnAction, 0, 5);
+		
+		this.setForAdd();
 	}
 	
 	public void update(Account account) {
-		this.account = account;
 		this.tfServer.setText(account==null ? "" : account.getServer());
 		this.tfUsername.setText(account==null ? "" : account.getUsername());
 		this.tfPassword.setText(account==null ? "" : account.getPassword());
@@ -70,25 +67,29 @@ public class AccountView extends GridPane{
 	}
 	
 	public void setForAdd() {
-		this.update(null);
+		if(this.btnAction.getText().equals("Add")){
+			this.update(null);
+		}
 		this.btnAction.setText("Add");
-		this.actionButton.setText("Edit");
 		this.btnAction.setOnAction(e -> {
+			if(this.tfServer.getText().length()==0){
+				return;
+			}
 			Account acc = new Account(this.tfServer.getText(),this.tfUsername.getText(),this.tfPassword.getText(),this.tfEmail.getText(),this.tfOther.getText());
 			acc.setID();
 			this.accountController.addAccount(acc);
 		});
 	}
 	
-	public void setForEdit() {
+	public void setForEdit(Account account) {
 		this.btnAction.setText("Edit");
-		this.actionButton.setText("Add");
 		this.btnAction.setOnAction(e -> {
-			Account acc = new Account(this.tfServer.getText(),this.tfUsername.getText(),this.tfPassword.getText(),this.tfEmail.getText(),this.tfOther.getText());
-			long accId = account.getID();
-			acc.setID(accId);
-			this.accountController.editAccount(this.account, acc);
-			this.account = acc;
+			if(account!=null){
+				Account acc = new Account(this.tfServer.getText(),this.tfUsername.getText(),this.tfPassword.getText(),this.tfEmail.getText(),this.tfOther.getText());
+				long accId = account.getID();
+				acc.setID(accId);
+				this.accountController.editAccount(account, acc);
+			}
 		});
 	}
 	
